62e9589cc8d9c9739e9fea9ee81a7a828f946a86
skodape <p.skoda@centrum.cz>
2014-05-15 09:53:53 +0200

    Extractor_ares update for SimpleRDF

--- ares/src/main/java/cz/opendata/linked/ares/Extractor.java
+++ ares/src/main/java/cz/opendata/linked/ares/Extractor.java
@@ -30,7 +30,9 @@ import cz.cuni.mff.xrg.odcs.commons.module.dpu.ConfigurableBase;
 import cz.cuni.mff.xrg.odcs.commons.web.AbstractConfigDialog;
 import cz.cuni.mff.xrg.odcs.commons.web.ConfigDialogProvider;
 import cz.cuni.mff.xrg.odcs.rdf.RDFDataUnit;
+import cz.cuni.mff.xrg.odcs.rdf.WritableRDFDataUnit;
 import cz.cuni.mff.xrg.odcs.rdf.simple.AddPolicy;
+import cz.cuni.mff.xrg.odcs.rdf.simple.OperationFailedException;
 import cz.cuni.mff.xrg.odcs.rdf.simple.SimpleRDF;
 import cz.cuni.mff.xrg.scraper.css_parser.utils.BannedException;
 import cz.cuni.mff.xrg.scraper.css_parser.utils.Cache;
@@ -90,7 +92,7 @@ implements DPU, ConfigDialogProvider<ExtractorConfig> {
 	}
 
 	@Override
-	public void execute(DPUContext ctx) throws DPUException
+	public void execute(DPUContext ctx) throws DPUException, OperationFailedException
 	{	
 		Cache.setInterval(config.getInterval());
 		Cache.setTimeout(config.getTimeout());
