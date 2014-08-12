851068b6c36de90761722025a08abbe538978afa
skodape <p.skoda@centrum.cz>
2014-08-12 22:19:18 +0200

    New tool script for initialization

---
+++ tools/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# build base
+echo "building base projects .."
+cd ../base 
+ cd unifiedviews-base
+  mvn clean install
+ cd ../unifiedviews-dpu-base
+  mvn clean install
+ cd ../unifiedviews-lib-base
+  mvn clean install
+ cd ..
+
+ # build libs
+echo "building libs .."
+cd ../libs
+ for f in *; do
+   cd $f
+    mvn clean install
+   cd ..
+  done
+
+# build template
+echo "preparing dpu template.."
+cd ../templates
+ cd dpu
+  mvn clean install
+ cd ..
+
+# go back to tools
+cd ../tools
+echo "done"
