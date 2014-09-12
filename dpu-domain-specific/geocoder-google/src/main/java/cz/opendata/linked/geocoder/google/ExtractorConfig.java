842d70dd9a64bb473629cbdf1435bd173021c135
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2014-09-12 13:39:23 +0200

    Google Geocoder UV1.3 update

--- dpu-domain-specific/geocoder-google/src/main/java/cz/opendata/linked/geocoder/google/ExtractorConfig.java
+++ dpu-domain-specific/geocoder-google/src/main/java/cz/opendata/linked/geocoder/google/ExtractorConfig.java
@@ -1,49 +1,43 @@
 package cz.opendata.linked.geocoder.google;
 
-import cz.cuni.mff.xrg.odcs.commons.module.config.DPUConfigObjectBase;
 
 /**
  *
  * Put your DPU's configuration here.
  *
  */
-public class ExtractorConfig extends DPUConfigObjectBase {
-	
-	private static final long serialVersionUID = 8719241993054209502L;
+public class ExtractorConfig  {
+    
+    private static final long serialVersionUID = 8719241993054209502L;
 
-	private int limit = 2400;
-	
-	private int interval = 1000;
-	
+    private int limit = 2400;
+    
+    private int interval = 1000;
+    
     private int hoursToCheck = 24;
-	
-	@Override
-    public boolean isValid() {
-		return true;
+    
+    public int getLimit() {
+        return limit;
     }
 
-	public int getLimit() {
-		return limit;
-	}
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
+    public int getInterval() {
+        return interval;
+    }
 
-	public int getInterval() {
-		return interval;
-	}
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
 
-	public void setInterval(int interval) {
-		this.interval = interval;
-	}
+    public int getHoursToCheck() {
+        return hoursToCheck;
+    }
 
-	public int getHoursToCheck() {
-		return hoursToCheck;
-	}
-
-	public void setHoursToCheck(int hoursToCheck) {
-		this.hoursToCheck = hoursToCheck;
-	}
+    public void setHoursToCheck(int hoursToCheck) {
+        this.hoursToCheck = hoursToCheck;
+    }
 
 }
--- dpu-domain-specific/geocoder-google/src/main/java/cz/opendata/linked/geocoder/google/ExtractorDialog.java
+++ dpu-domain-specific/geocoder-google/src/main/java/cz/opendata/linked/geocoder/google/ExtractorDialog.java
@@ -4,31 +4,32 @@ import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 
-import cz.cuni.mff.xrg.odcs.commons.configuration.ConfigException;
-import cz.cuni.mff.xrg.odcs.commons.module.dialog.BaseConfigDialog;
+import eu.unifiedviews.dpu.config.DPUConfigException;
+import cz.cuni.mff.xrg.uv.boost.dpu.addon.AddonInitializer;
+import cz.cuni.mff.xrg.uv.boost.dpu.gui.AdvancedVaadinDialogBase;
 
 /**
  * DPU's configuration dialog. User can use this dialog to configure DPU configuration.
  *
  */
-public class ExtractorDialog extends BaseConfigDialog<ExtractorConfig> {
+public class ExtractorDialog extends AdvancedVaadinDialogBase<ExtractorConfig> {
 
-	private static final long serialVersionUID = 7003725620084616056L;
+    private static final long serialVersionUID = 7003725620084616056L;
 
-	private GridLayout mainLayout;
+    private GridLayout mainLayout;
     private TextField interval;
     private TextField limit;
     private TextField hoursToCheck;
 
-	public ExtractorDialog() {
-		super(ExtractorConfig.class);
+    public ExtractorDialog() {
+        super(ExtractorConfig.class,AddonInitializer.noAddons());
         buildMainLayout();
-		Panel panel = new Panel();
-		panel.setSizeFull();
-		panel.setContent(mainLayout);
-		setCompositionRoot(panel);
+        Panel panel = new Panel();
+        panel.setSizeFull();
+        panel.setContent(mainLayout);
+        setCompositionRoot(panel);
     }  
-	
+    
     private GridLayout buildMainLayout() {
         // common part: create layout
         mainLayout = new GridLayout(1, 2);
@@ -55,23 +56,23 @@ public class ExtractorDialog extends BaseConfigDialog<ExtractorConfig> {
         mainLayout.addComponent(hoursToCheck);
 
         return mainLayout;
-    }	
+    }    
      
-	@Override
-	public void setConfiguration(ExtractorConfig conf) throws ConfigException {
-		interval.setValue(Integer.toString(conf.getInterval()));
-		hoursToCheck.setValue(Integer.toString(conf.getHoursToCheck()));
-		limit.setValue(Integer.toString(conf.getLimit()));
+    @Override
+    public void setConfiguration(ExtractorConfig conf) throws DPUConfigException {
+        interval.setValue(Integer.toString(conf.getInterval()));
+        hoursToCheck.setValue(Integer.toString(conf.getHoursToCheck()));
+        limit.setValue(Integer.toString(conf.getLimit()));
 
-	}
+    }
 
-	@Override
-	public ExtractorConfig getConfiguration() throws ConfigException {
-		ExtractorConfig conf = new ExtractorConfig();
-		conf.setInterval(Integer.parseInt(interval.getValue()));
-		conf.setHoursToCheck(Integer.parseInt(hoursToCheck.getValue()));
-		conf.setLimit(Integer.parseInt(limit.getValue()));
-		return conf;
-	}
-	
+    @Override
+    public ExtractorConfig getConfiguration() throws DPUConfigException {
+        ExtractorConfig conf = new ExtractorConfig();
+        conf.setInterval(Integer.parseInt(interval.getValue()));
+        conf.setHoursToCheck(Integer.parseInt(hoursToCheck.getValue()));
+        conf.setLimit(Integer.parseInt(limit.getValue()));
+        return conf;
+    }
+    
 }
