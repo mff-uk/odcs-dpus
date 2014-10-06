76535f62d4489b7fce0129bd56059e4bdcbe690d
skodape <p.skoda@centrum.cz>
2014-10-06 14:52:37 +0200

    DPUs use ComponentTable instead of GeneratedTable

--- dpu/e-httpDownload/src/main/java/cz/cuni/mff/xrg/uv/extractor/httpdownload/HttpDownloadVaadinDialog.java
+++ dpu/e-httpDownload/src/main/java/cz/cuni/mff/xrg/uv/extractor/httpdownload/HttpDownloadVaadinDialog.java
@@ -1,74 +1,58 @@
 package cz.cuni.mff.xrg.uv.extractor.httpdownload;
 
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
 import cz.cuni.mff.xrg.uv.boost.dpu.addon.AddonInitializer;
 import cz.cuni.mff.xrg.uv.boost.dpu.addon.impl.CachedFileDownloader;
 import cz.cuni.mff.xrg.uv.boost.dpu.addon.impl.ConfigurationFromRdf;
 import cz.cuni.mff.xrg.uv.boost.dpu.config.ConfigHistory;
 import cz.cuni.mff.xrg.uv.boost.dpu.gui.AdvancedVaadinDialogBase;
+import cz.cuni.mff.xrg.uv.utils.dialog.container.ComponentTable;
+import cz.cuni.mff.xrg.uv.utils.dialog.validator.UrlValidator;
 import eu.unifiedviews.dpu.config.DPUConfigException;
 
 public class HttpDownloadVaadinDialog extends AdvancedVaadinDialogBase<HttpDownloadConfig_V2> {
 
-    private TextField txtSourceUrl;
-
-    private TextField txtTarget;
+    private ComponentTable<DownloadInfo_V1> table;
 
     public HttpDownloadVaadinDialog() {
         super(ConfigHistory.create(HttpDownloadConfig_V1.class,
                 "eu.unifiedviews.plugins.extractor.httpdownload.HttpDownloadConfig_V1")
                 .addCurrent(HttpDownloadConfig_V2.class),
-                AddonInitializer.create(new CachedFileDownloader(), new ConfigurationFromRdf("inRdfToDownload")));
+                AddonInitializer.create(new CachedFileDownloader(),
+                        new ConfigurationFromRdf("inRdfToDownload")));
 
         buildLayout();
     }
 
     @Override
     public void setConfiguration(HttpDownloadConfig_V2 c) throws DPUConfigException {
-        if (c.getToDownload().size() > 0) {
-            txtSourceUrl.setValue(c.getToDownload().get(0).getUri());
-            txtTarget.setValue(c.getToDownload().get(0).getVirtualPath());
-        }
+        table.setValue(c.getToDownload());
     }
 
     @Override
     public HttpDownloadConfig_V2 getConfiguration() throws DPUConfigException {
         final HttpDownloadConfig_V2 c = new HttpDownloadConfig_V2();
-
-        // use conversion by URI!
-        c.getToDownload().add(new DownloadInfo_V1(
-                txtSourceUrl.getValue(),
-                txtTarget.getValue()));
-
+        c.getToDownload().addAll(table.getValue());
         return c;
     }
 
     private void buildLayout() {
-		setWidth("100%");
-		setHeight("100%");
-        
-        final VerticalLayout mainLayout = new VerticalLayout();
-        mainLayout.setWidth("100%");
-        mainLayout.setHeight("-1px");
-        mainLayout.setMargin(true);
-        mainLayout.setMargin(true);
+        setSizeFull();
 
-		txtSourceUrl = new TextField();
-		txtSourceUrl.setWidth("100%");
-		txtSourceUrl.setHeight("-1px");
-		txtSourceUrl.setCaption("URL:");
-		txtSourceUrl.setRequired(false);
-		txtSourceUrl.setNullRepresentation("");
-		mainLayout.addComponent(txtSourceUrl);
+        table = new ComponentTable<>(DownloadInfo_V1.class,
+                new ComponentTable.ColumnInfo("uri", "Uri", new UrlValidator(), 0.5f),
+                new ComponentTable.ColumnInfo("virtualPath", "Download as (optional)", null, 0.5f)
+        );
 
-		txtTarget = new TextField();
-		txtTarget.setWidth("100%");
-		txtTarget.setHeight("-1px");
-		txtTarget.setCaption("Target - file name and location in output:");
-		txtTarget.setRequired(true);
-		mainLayout.addComponent(txtTarget);
+        table.setPolicy(new ComponentTable.Policy<DownloadInfo_V1>() {
 
-        setCompositionRoot(mainLayout);
+            @Override
+            public boolean isSet(DownloadInfo_V1 value) {
+                return value.getUri() != null && !value.getUri().isEmpty();
+            }
+
+        });
+
+        setCompositionRoot(table
+        );
     }
 }
--- dpu/t-htmlCss/src/main/java/cz/cuni/mff/xrg/uv/transformer/HtmlCssVaadinDialog.java
+++ dpu/t-htmlCss/src/main/java/cz/cuni/mff/xrg/uv/transformer/HtmlCssVaadinDialog.java
@@ -4,12 +4,13 @@ import com.vaadin.ui.VerticalLayout;
 import cz.cuni.mff.xrg.uv.boost.dpu.addon.AddonInitializer;
 import cz.cuni.mff.xrg.uv.boost.dpu.addon.impl.SimpleRdfConfigurator;
 import cz.cuni.mff.xrg.uv.boost.dpu.gui.AdvancedVaadinDialogBase;
-import cz.cuni.mff.xrg.uv.utils.dialog.container.GeneratedTable;
+import cz.cuni.mff.xrg.uv.utils.dialog.container.ComponentTable;
+import cz.cuni.mff.xrg.uv.utils.dialog.validator.UrlValidator;
 import eu.unifiedviews.dpu.config.DPUConfigException;
 
 public class HtmlCssVaadinDialog extends AdvancedVaadinDialogBase<HtmlCssConfig_V1> {
 
-    private GeneratedTable genTable;
+    private ComponentTable genTable;
 
     public HtmlCssVaadinDialog() {
         super(HtmlCssConfig_V1.class,
@@ -30,32 +31,24 @@ public class HtmlCssVaadinDialog extends AdvancedVaadinDialogBase<HtmlCssConfig_
         return c;
     }
 
-    VerticalLayout layout;
-
     private void buildLayout() {
         setSizeFull();
 
-        layout = new VerticalLayout();
-        layout.setSizeFull();
+        genTable = new ComponentTable(HtmlCssConfig_V1.Query.class,
+                new ComponentTable.ColumnInfo("query", "Jsoup query", null, 0.4f),
+                new ComponentTable.ColumnInfo("type", "Type", null, 0.1f),
+                new ComponentTable.ColumnInfo("predicate", "Predicate", new UrlValidator(false), 0.2f),
+                new ComponentTable.ColumnInfo("attrName", "Attribute name", null, 0.1f));
 
-        genTable = new GeneratedTable(HtmlCssConfig_V1.Query.class,
-                new GeneratedTable.Policy<HtmlCssConfig_V1.Query>() {
+        genTable.setPolicy(new ComponentTable.Policy<HtmlCssConfig_V1.Query>() {
 
-                    @Override
-                    public boolean isSet(HtmlCssConfig_V1.Query value) {
-                        return value.getQuery() != null && !value.getQuery().isEmpty();
-                    }
+            @Override
+            public boolean isSet(HtmlCssConfig_V1.Query value) {
+                return value.getQuery() != null && !value.getQuery().isEmpty();
+            }
 
-                });
-        
-        genTable.setMargin(true);
-        genTable.setColumn(
-                new GeneratedTable.ColumnInfo("query", "Jsoup query"),
-                new GeneratedTable.ColumnInfo("type", "Type"),
-                new GeneratedTable.ColumnInfo("predicate", "Predicate"),
-                new GeneratedTable.ColumnInfo("attrName", "Attribute name"));
+        });
 
-        layout.addComponent(genTable);
-        setCompositionRoot(layout);
+        setCompositionRoot(genTable);
     }
 }
