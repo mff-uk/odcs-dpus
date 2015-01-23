b948f3bbbbb5f99e66c3e06fa17bbe8b9f5a70be
skodape <p.skoda@centrum.cz>
2015-01-23 16:35:35 +0100

    SparqlQuery Merged into SparqlUtils

---
+++ libs/utils-dataunit-rdf/src/main/java/cz/cuni/mff/xrg/uv/rdf/utils/dataunit/rdf/sparql/SparqlProblemException.java
@@ -0,0 +1,23 @@
+package cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql;
+
+import cz.cuni.mff.xrg.uv.utils.dataunit.FailedOperationException;
+
+/**
+ *
+ * @author Škoda Petr
+ */
+public class SparqlProblemException extends FailedOperationException {
+
+    public SparqlProblemException(String message) {
+        super(message);
+    }
+
+    public SparqlProblemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SparqlProblemException(Throwable cause) {
+        super(cause);
+    }
+
+}
--- libs/utils-dataunit-rdf/src/main/java/cz/cuni/mff/xrg/uv/rdf/utils/dataunit/rdf/sparql/SparqlUtils.java
+++ libs/utils-dataunit-rdf/src/main/java/cz/cuni/mff/xrg/uv/rdf/utils/dataunit/rdf/sparql/SparqlUtils.java
@@ -2,17 +2,16 @@ package cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql;
 
 import java.util.List;
 
-import org.openrdf.query.BooleanQuery;
-import org.openrdf.query.MalformedQueryException;
-import org.openrdf.query.QueryEvaluationException;
-import org.openrdf.query.QueryLanguage;
-import org.openrdf.query.UpdateExecutionException;
+import org.openrdf.model.Statement;
+import org.openrdf.query.*;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.rdf.RDFDataUnit;
+import eu.unifiedviews.dpu.DPUContext;
+import eu.unifiedviews.dpu.DPUException;
 
 /**
  * Utilities for SPARQL execution.
@@ -77,6 +76,60 @@ public class SparqlUtils {
 
     }
 
+    /**
+     * Represents SPARQL select query.
+     */
+    public static class SparqlSelectObject extends SparqlQueryObject {
+
+        public SparqlSelectObject(String sparql) {
+            super(sparql);
+        }
+
+    }
+
+    /**
+     * Represents SPARQL select query.
+     */
+    public static class SparqlConstructObject extends SparqlQueryObject {
+
+        public SparqlConstructObject(String sparql) {
+            super(sparql);
+        }
+
+    }
+
+    /**
+     * User callback for result iteration.
+     */
+    public interface TupleIterator {
+
+        /**
+         * Called for every tuple in result.
+         *
+         * @param binding
+         * @throws eu.unifiedviews.dpu.DPUException If thrown the the iteration stops and the the same
+         *                                          exception is re-thrown.
+         */
+        public void next(BindingSet binding) throws DPUException;
+
+    }
+
+    /**
+     * User callback for result iteration.
+     */
+    public interface StatementIterator {
+
+        /**
+         * Called for every tuple in result.
+         *
+         * @param binding
+         * @throws eu.unifiedviews.dpu.DPUException If thrown the the iteration stops and the the same
+         *                                          exception is re-thrown.
+         */
+        public void next(Statement binding) throws DPUException;
+
+    }
+
     private SparqlUtils() {
         
     }
@@ -102,13 +155,13 @@ public class SparqlUtils {
      * @param clause
      * @param entries
      * @return
-     * @throws SparqlUtilsException Is thrown in ma number of graph is exceeded.
+     * @throws SparqlProblemException Is thrown in ma number of graph is exceeded.
      * @throws eu.unifiedviews.dataunit.DataUnitException
      */
     public static String prepareClause(String clause, List<RDFDataUnit.Entry> entries)
-            throws SparqlUtilsException, DataUnitException {
+            throws SparqlProblemException, DataUnitException {
         if (entries.size() > MAX_GRAPH_COUNT) {
-            throw new SparqlUtilsException(String.format("Maximum graph limit exceeded. %d limit: %d",
+            throw new SparqlProblemException(String.format("Maximum graph limit exceeded. %d limit: %d",
                     entries.size(), MAX_GRAPH_COUNT));
         }
 
@@ -128,11 +181,11 @@ public class SparqlUtils {
      * @param sources
      * @param target
      * @return Prepared SPARQL update query.
-     * @throws cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql.SparqlUtilsException
+     * @throws cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql.SparqlProblemException
      * @throws eu.unifiedviews.dataunit.DataUnitException
      */
-    public static SparqlUpdateObject createInsert(String query, List<RDFDataUnit.Entry> sources, RDFDataUnit.Entry target)
-            throws SparqlUtilsException, DataUnitException {
+    public static SparqlUpdateObject createInsert(String query, List<RDFDataUnit.Entry> sources, 
+            RDFDataUnit.Entry target) throws SparqlProblemException, DataUnitException {
         query = query.replaceFirst("(?i)INSERT", prepareClause("WITH", target) + "INSERT");
         query = query.replaceFirst("(?i)WHERE", prepareClause("USING", sources) + "WHERE");
         // Return new object.
@@ -144,11 +197,11 @@ public class SparqlUtils {
      * @param query
      * @param entries
      * @return Prepared SPARQL update query.
-     * @throws cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql.SparqlUtilsException
+     * @throws cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql.SparqlProblemException
      * @throws eu.unifiedviews.dataunit.DataUnitException
      */
     public static SparqlAskObject createAsk(String query, List<RDFDataUnit.Entry> entries)
-            throws SparqlUtilsException, DataUnitException {
+            throws SparqlProblemException, DataUnitException {
         query = query.replaceFirst("(?i)ASK", "ASK " + prepareClause("FROM", entries) + "WHERE ");
         // Return new object.
         return new SparqlAskObject(query);
@@ -185,6 +238,94 @@ public class SparqlUtils {
         LOG.debug("Executing ask: {}", askObject.sparqlQuery);
         final BooleanQuery query = connection.prepareBooleanQuery(QueryLanguage.SPARQL, askObject.sparqlQuery);
         askObject.result = query.evaluate();
+    }
+
+    /**
+     * Execute given query and call use call-back for each tuple. This function does not provide any fault
+     * tolerance, the iteration may fail at any time.
+     *
+     * Does not close given connection.
+     *
+     * @param connection
+     * @param context
+     * @param queryObject
+     * @param callback Must not be null.
+     * @throws SparqlProblemException
+     * @throws MalformedQueryException
+     * @throws DPUException
+     */
+    public static void execute(RepositoryConnection connection, DPUContext context, 
+            SparqlSelectObject queryObject, TupleIterator callback)
+            throws SparqlProblemException, MalformedQueryException, DPUException {
+        LOG.debug("Executing query: {}", queryObject.sparqlQuery);
+        // Prepare query.
+        final TupleQuery query;
+        try {
+            query = connection.prepareTupleQuery(QueryLanguage.SPARQL, queryObject.sparqlQuery);
+        } catch (RepositoryException ex) {
+            throw new SparqlProblemException("Can't prepare query because of problem with repository.", ex);
+        }
+        // Execute and iterate over result.
+        TupleQueryResult queryResult = null;
+        try {
+            queryResult = query.evaluate();
+            while (queryResult.hasNext() && !context.canceled()) {
+                callback.next(queryResult.next());
+            }
+        } catch (QueryEvaluationException ex) {
+            throw new SparqlProblemException("Can't evaluate query.", ex);
+        } finally {
+            try {
+                if (queryResult != null) {
+                    queryResult.close();
+                }
+            } catch (QueryEvaluationException ex) {
+                LOG.warn("Can't close query result.", ex);
+            }
+        }        
+    }
+
+    /**
+     * Execute user given construct.
+     *
+     * @param connection
+     * @param context
+     * @param constructObject
+     * @param callback Use null to not iterate over result.
+     * @throws cz.cuni.mff.xrg.uv.rdf.utils.dataunit.rdf.sparql.SparqlProblemException
+     * @throws MalformedQueryException
+     */
+     public static void execute(RepositoryConnection connection, DPUContext context,
+             SparqlConstructObject constructObject, StatementIterator callback)
+            throws SparqlProblemException, MalformedQueryException, DPUException {
+        LOG.debug("Executing construct: {}", constructObject.sparqlQuery);
+        // Prepare query.
+        final GraphQuery query;
+        try {
+            query = connection.prepareGraphQuery(QueryLanguage.SPARQL, constructObject.sparqlQuery);
+        } catch (RepositoryException ex) {
+            throw new SparqlProblemException("Can't prepare query because of problem with repository.", ex);
+        }
+        // Execute and iterate over result.
+        GraphQueryResult queryResult = null;
+        try {
+            queryResult = query.evaluate();
+            if (callback != null) {
+                while (queryResult.hasNext() && !context.canceled()) {
+                    callback.next(queryResult.next());
+                }
+            }
+        } catch (QueryEvaluationException ex) {
+            throw new SparqlProblemException("Can't evaluate query.", ex);
+        } finally {
+            try {
+                if (queryResult != null) {
+                    queryResult.close();
+                }
+            } catch (QueryEvaluationException ex) {
+                LOG.warn("Can't close query result.", ex);
+            }
+        }
     }
 
 }
