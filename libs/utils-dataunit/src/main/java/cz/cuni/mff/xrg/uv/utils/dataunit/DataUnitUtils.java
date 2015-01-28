4817a6f8f051b7bd98572ac7766a83cc446c2ec3
skodape <p.skoda@centrum.cz>
2015-01-28 15:54:27 +0100

    Removed method copyEntries from DataUnitUtils, no DPU should need this method

--- libs/utils-dataunit/src/main/java/cz/cuni/mff/xrg/uv/utils/dataunit/DataUnitUtils.java
+++ libs/utils-dataunit/src/main/java/cz/cuni/mff/xrg/uv/utils/dataunit/DataUnitUtils.java
@@ -3,17 +3,9 @@ package cz.cuni.mff.xrg.uv.utils.dataunit;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
-import org.openrdf.model.URI;
-import org.openrdf.query.MalformedQueryException;
-import org.openrdf.query.QueryLanguage;
-import org.openrdf.query.UpdateExecutionException;
-import org.openrdf.repository.RepositoryConnection;
-import org.openrdf.repository.RepositoryException;
 import eu.unifiedviews.dataunit.DataUnitException;
 import eu.unifiedviews.dataunit.MetadataDataUnit;
-import eu.unifiedviews.dataunit.WritableMetadataDataUnit;
 
 /**
  *
@@ -60,34 +52,6 @@ public class DataUnitUtils {
             }
         }
         return result;
-    }
-
-    /**
-     * Copy content of metadata graphs from input to output.
-     *
-     * @param sourceDataUnit
-     * @param targetDataUnit
-     * @param connection
-     * @throws DataUnitException
-     * @throws RepositoryException
-     * @throws FailedOperationException
-     */
-    public static void copyEntries(MetadataDataUnit sourceDataUnit,
-            WritableMetadataDataUnit targetDataUnit, RepositoryConnection connection)
-            throws DataUnitException, RepositoryException, FailedOperationException {
-        final Set<URI> metadatagraphs = sourceDataUnit.getMetadataGraphnames();
-        final URI target = targetDataUnit.getMetadataWriteGraphname();
-        // Copy data from each source graph to target graph.
-        for (URI source : metadatagraphs) {
-            final String query = String.format("ADD <%s> TO <%s>", source.stringValue(), target.stringValue());
-            try {
-                connection.prepareUpdate(QueryLanguage.SPARQL, query).execute();
-            } catch (MalformedQueryException ex) {
-                throw new RuntimeException("Harcoded query is malformed!" , ex);
-            } catch (UpdateExecutionException ex) {
-                throw new FailedOperationException("Can't copy metadata.", ex);
-            }
-        }
     }
 
 }
