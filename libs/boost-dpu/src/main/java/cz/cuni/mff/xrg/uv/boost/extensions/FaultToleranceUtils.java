8182c06157af787388c9ff542bbd5cd508ac9d66
skodape <p.skoda@centrum.cz>
2015-02-05 10:44:20 +0100

    GetValueFactory funciton removed from FaultToleranceUtils as SimpleRdf is alredy fault tolerant if FaultTolerance is presented

--- libs/boost-dpu/src/main/java/cz/cuni/mff/xrg/uv/boost/extensions/FaultToleranceUtils.java
+++ libs/boost-dpu/src/main/java/cz/cuni/mff/xrg/uv/boost/extensions/FaultToleranceUtils.java
@@ -1,10 +1,5 @@
 package cz.cuni.mff.xrg.uv.boost.extensions;
 
-import org.openrdf.model.ValueFactory;
-
-import cz.cuni.mff.xrg.uv.boost.rdf.simple.SimpleRdf;
-import eu.unifiedviews.dpu.DPUException;
-
 /**
  * Contains code for common operation with {@link FaultTolerance}.
  *
@@ -14,25 +9,6 @@ public class FaultToleranceUtils {
 
     private FaultToleranceUtils() {
 
-    }
-
-    /**
-     *
-     * @param faultTolerance
-     * @param simpleRdf
-     * @return {@link ValueFactory} obtained from given {@link SimpleRdf} instance.
-     * @throws DPUException
-     */
-    public static ValueFactory getValuFactory(FaultTolerance faultTolerance,
-            final SimpleRdf simpleRdf) throws DPUException {
-        return faultTolerance.execute(
-                new FaultTolerance.ActionReturn<ValueFactory>() {
-
-                    @Override
-                    public ValueFactory action() throws Exception {
-                        return simpleRdf.getValueFactory();
-                    }
-                });
     }
 
 }
