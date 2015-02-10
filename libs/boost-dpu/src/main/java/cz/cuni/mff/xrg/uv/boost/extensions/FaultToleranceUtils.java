3b7a4852ce6c12e64c10a187904beb45f3b2e8ea
skodape <p.skoda@centrum.cz>
2015-02-10 09:10:04 +0100

    Added methods into FaultToleranceUtils class

--- libs/boost-dpu/src/main/java/cz/cuni/mff/xrg/uv/boost/extensions/FaultToleranceUtils.java
+++ libs/boost-dpu/src/main/java/cz/cuni/mff/xrg/uv/boost/extensions/FaultToleranceUtils.java
@@ -1,5 +1,14 @@
 package cz.cuni.mff.xrg.uv.boost.extensions;
 
+import java.io.File;
+import java.util.List;
+
+import cz.cuni.mff.xrg.uv.utils.dataunit.DataUnitUtils;
+import cz.cuni.mff.xrg.uv.utils.dataunit.files.FilesDataUnitUtils;
+import eu.unifiedviews.dataunit.MetadataDataUnit;
+import eu.unifiedviews.dataunit.files.FilesDataUnit;
+import eu.unifiedviews.dpu.DPUException;
+
 /**
  * Contains code for common operation with {@link FaultTolerance}.
  *
@@ -10,5 +19,48 @@ public class FaultToleranceUtils {
     private FaultToleranceUtils() {
 
     }
+
+    /**
+     * Eager load entries into list.
+     *
+     * @param <T>
+     * @param <E>
+     * @param faultTolerance
+     * @param dataUnit
+     * @param resultClass
+     * @return
+     * @throws DPUException
+     */
+    public static <T extends MetadataDataUnit, E extends T.Entry> List<E> getEntries(
+            FaultTolerance faultTolerance, final T dataUnit, final Class<E> resultClass) throws DPUException {
+
+        return faultTolerance.execute(new FaultTolerance.ActionReturn<List<E>>() {
+
+            @Override
+            public List<E> action() throws Exception {
+                return DataUnitUtils.getEntries(dataUnit, resultClass);
+            }
+        });
+    }
+
+    /**
+     * Convert {@link FilesDataUnit.Entry} to {@link File}
+     *
+     * @param faultTolerance
+     * @param entry
+     * @return
+     * @throws DPUException
+     */
+    public static File asFile(FaultTolerance faultTolerance, final FilesDataUnit.Entry entry)
+            throws DPUException {
+        return faultTolerance.execute(new FaultTolerance.ActionReturn<File>() {
+
+            @Override
+            public File action() throws Exception {
+                return FilesDataUnitUtils.asFile(entry);
+            }
+        });
+    }
+
 
 }
