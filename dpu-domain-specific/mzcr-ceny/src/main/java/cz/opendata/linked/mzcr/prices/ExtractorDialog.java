e764a18223ddcaeda9e3f96464b0e0ec31a5d928
skodape <p.skoda@centrum.cz>
2015-03-16 09:01:32 +0100

    ext. mzcd prices updated

--- dpu-domain-specific/mzcr-ceny/src/main/java/cz/opendata/linked/mzcr/prices/ExtractorDialog.java
+++ dpu-domain-specific/mzcr-ceny/src/main/java/cz/opendata/linked/mzcr/prices/ExtractorDialog.java
@@ -4,31 +4,30 @@ import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.ui.*;
 
 import eu.unifiedviews.dpu.config.DPUConfigException;
-import cz.cuni.mff.xrg.uv.boost.dpu.addon.AddonInitializer;
-import cz.cuni.mff.xrg.uv.boost.dpu.addon.impl.SimpleRdfConfigurator;
-import cz.cuni.mff.xrg.uv.boost.dpu.gui.AdvancedVaadinDialogBase;
+import eu.unifiedviews.helpers.dpu.vaadin.dialog.AbstractDialog;
 
 /**
  * DPU's configuration dialog. User can use this dialog to configure DPU configuration.
  *
  */
-public class ExtractorDialog extends AdvancedVaadinDialogBase<ExtractorConfig> {
+public class ExtractorDialog extends AbstractDialog<ExtractorConfig> {
+
+    private static final long serialVersionUID = -8158163219102623590L;
 
     private GridLayout mainLayout;
+
     private TextField interval;
+
     private TextField timeout;
+
     private CheckBox chkRewriteCache;
-    
+
     public ExtractorDialog() {
-        super(ExtractorConfig.class,AddonInitializer.create(new SimpleRdfConfigurator(Extractor.class)));
-        buildMainLayout();
-        Panel panel = new Panel();
-        panel.setSizeFull();
-        panel.setContent(mainLayout);
-        setCompositionRoot(panel);
-    }  
-    
-    private GridLayout buildMainLayout() {
+        super(Extractor.class);
+    }
+
+    @Override
+    protected void buildDialogLayout() {
         // common part: create layout
         mainLayout = new GridLayout(1, 2);
         mainLayout.setImmediate(false);
@@ -44,24 +43,22 @@ public class ExtractorDialog extends AdvancedVaadinDialogBase<ExtractorConfig> {
         chkRewriteCache = new CheckBox("Ignore (Rewrite) document cache:");
         chkRewriteCache.setDescription("When selected, documents cache will be ignored and rewritten.");
         chkRewriteCache.setWidth("100%");
-        
+
         mainLayout.addComponent(chkRewriteCache);
         interval = new TextField();
         interval.setCaption("Interval between downloads:");
         mainLayout.addComponent(interval);
-        
+
         timeout = new TextField();
         timeout.setCaption("Timeout for download:");
         mainLayout.addComponent(timeout);
-        
-        return mainLayout;
-    }    
-     
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -8158163219102623590L;
-    
+
+        Panel panel = new Panel();
+        panel.setSizeFull();
+        panel.setContent(mainLayout);
+        setCompositionRoot(panel);
+    }
+
     @Override
     public void setConfiguration(ExtractorConfig conf) throws DPUConfigException {
         chkRewriteCache.setValue(conf.isRewriteCache());
@@ -73,11 +70,19 @@ public class ExtractorDialog extends AdvancedVaadinDialogBase<ExtractorConfig> {
     public ExtractorConfig getConfiguration() throws DPUConfigException {
         ExtractorConfig conf = new ExtractorConfig();
         conf.setRewriteCache((boolean) chkRewriteCache.getValue());
-        try { Integer.parseInt(interval.getValue()); } catch (InvalidValueException e) { return conf;}
+        try {
+            Integer.parseInt(interval.getValue());
+        } catch (InvalidValueException e) {
+            return conf;
+        }
         conf.setInterval(Integer.parseInt(interval.getValue()));
-        try { Integer.parseInt(timeout.getValue()); } catch (InvalidValueException e) { return conf;}
+        try {
+            Integer.parseInt(timeout.getValue());
+        } catch (InvalidValueException e) {
+            return conf;
+        }
         conf.setTimeout(Integer.parseInt(timeout.getValue()));
         return conf;
     }
-    
+
 }
